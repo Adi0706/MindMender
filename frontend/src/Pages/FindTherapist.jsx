0523b4e1aed06ba687ef779aa1cc04974a5e86a6
Aditya Bhattacharjee <adityab76@gmail.com>
2023-09-11 15:18:07 +0530

    find your therapist page updated

--- frontend/src/Pages/FindTherapist.jsx
+++ frontend/src/Pages/FindTherapist.jsx
@@ -1,14 +1,140 @@
 import React from 'react';
 import NavBar from '../Components/NavBar';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import {LiaFilterSolid} from 'react-icons/lia';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+
 
 function FindTherapist() {
   return (
     <div>
       <NavBar />
+      
       <section className="dummy-div">
+      <h3 style={{marginLeft:"300px",marginTop:"10px",position:"absolute" }}>FILTERS <LiaFilterSolid/></h3>
+         <div className='functionality'>
+          <b>TYPE OF PROFESSIONAL  AVAILABLE:</b>
+         <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        '& > *': {
+          m: 1,
+        },
+      }}
+    >
+    
+      <ButtonGroup variant="text" aria-label="text button group">
+        <Button variant="outlined" sx={{color:"black"}}>COUNCELLOR </Button>
+        <Button  variant="outlined"sx={{color:"black"}}>HYPNOTHERAPIST</Button>
+        <Button  variant="outlined" sx={{color:"black"}}>LIFE COACH</Button>
+      </ButtonGroup>
+    </Box>
+
+    <b>SESSION TYPE : </b>
+    <Stack direction="row" spacing={1}>
+      <Button variant="outlined" sx={{color:"black"}}>OFFLINE</Button>
+     
+      <Button variant="outlined" sx={{color:"black"}}>
+        ONLNE
+      </Button>
+    </Stack>
+          
+         </div>
+
         <div className="selection">
-          HELLO
-        </div>
+      <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card> <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    </div>
       </section>
     </div>
   );
